4d44888a772dc8e0401bbadbc25c009959687795
Sarrthi <sarrthijasrotia05@gmail.com>
2022-10-31 08:57:52 -0500

    project 3 cardadded

--- src/Components/Contact.js
+++ src/Components/Contact.js
@@ -24,8 +24,8 @@ const Contact = () => {
       
       
       <div className='contact-icons'>
-      <a href='https://github.com/SarrthiJasrotia'><img src={Github}/></a> 
-      <a href='https://www.linkedin.com/in/sarrthi-jasrotia/'><img src={Linkedin}/></a> 
+      <a href='https://github.com/SarrthiJasrotia' target='_blank'><img src={Github}/></a> 
+      <a href='https://www.linkedin.com/in/sarrthi-jasrotia/' target='_blank'><img src={Linkedin}/></a> 
       </div>
 
 
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -2,15 +2,26 @@ import Cards from "./Cards";
 import { Flip,Fade } from "react-reveal";
 import CoTeach from '../Images/Coteach.gif';
 import Itravelynot from '../Images/Itravelynot.gif';
-
+import TravelBlog from '../Images/TravelBlog.gif'
 const Project =()=>(
   <div className="project">
     <Flip duration={1500}bottom><div className="title">Projects</div></Flip>
   <Fade delay={500} duration={2000}><div className="projectGrid">
+    {/* project 1 */}
   <Cards img={CoTeach} title={'CoTeach'} github={'https://github.com/SarrthiJasrotia/CoTeach_frontend.git'} app={'https://vocal-figolla-43a5cd.netlify.app/'} />
+  
+  {/* project 2 */}
   <Cards img={Itravelynot} title={'ITravelYNot'} github={'https://github.com/SarrthiJasrotia/Project_ITravelYNot.git'} app={'https://itravelynot.com/'} />
+  
+  {/* project 3 */}
+  <Cards img={TravelBlog} title={'TravelBlog'} github={'https://github.com/SarrthiJasrotia/Project_ITravelYNot.git'} app={'https://travelblog-362615.uc.r.appspot.com/'} />
+  
+  {/* project 4 */}
   <Cards img={''} title={''} github={''} app={''} />
-  <Cards img={''} title={''} github={''} app={''} />
+  {/* <Cards img={''} title={''} github={''} app={''} /> */}
+  {/* <Cards img={''} title={''} github={''} app={''} /> */}
+  {/* <Cards img={''} title={''} github={''} app={''} /> */}
+  {/* <Cards img={''} title={''} github={''} app={''} /> */}
   </div></Fade>
   </div>
 
