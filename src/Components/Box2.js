5e5c073b6b3cc1402e9caa1eb5126bbbf85beaf4
Sarrthi <sarrthijasrotia05@gmail.com>
2022-11-06 19:34:47 -0600

    added download resume btn

--- src/Components/Box2.js
+++ src/Components/Box2.js
@@ -1,5 +1,5 @@
 import { Parallax } from "react-parallax";
-import { Zoom,Bounce } from "react-reveal";
+import { Zoom, Bounce } from "react-reveal";
 import Nav from "./Nav";
 import AboutMe from "./AboutMe";
 import TechStack from "./TechStack";
@@ -7,29 +7,32 @@ import Project from "./Projects";
 import Resume from "./Resume";
 import Contact from "./Contact";
 
-const Box2 =()=>(
-    <Zoom duration={1500}>
-    <Parallax className="box2" strength={1000}>
-       
-        <div className="content2">
-              <Nav/>
-            
-                <div className="box2column">
-                <Bounce right delay={1500} duration={2000}> <div className="columnDiv1"><AboutMe/></div></Bounce>
-                <Bounce left delay={1500} duration={2000}><div className="columnDiv2"><TechStack/></div></Bounce>
-             </div>
-             
-                
-        </div>
+const Box2 = () => {
 
-        <Resume/>
-        <Project/>
-        
-        <Contact/>
-        
-    </Parallax>
-    </Zoom>
+   
+    return (
+        <Zoom duration={1500}>
+            <Parallax className="box2" strength={1000}>
 
-)
+                <div className="content2">
+                    <Nav />
+
+                    <div className="box2column">
+                        <Bounce right delay={1500} duration={2000}> <div className="columnDiv1"><AboutMe /></div></Bounce>
+                        <Bounce left delay={1500} duration={2000}><div className="columnDiv2"><TechStack /></div></Bounce>
+                    </div>
+
+
+                </div>
+
+                <Resume />
+                <Project />
+
+                <Contact />
+
+            </Parallax>
+        </Zoom>
+    )
+}
 
 export default Box2
--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,9 +1,33 @@
 import React from 'react'
-import { Flip,Fade } from "react-reveal";
+import { Flip, Fade } from "react-reveal";
 const Resume = () => {
+  // Function will execute on click of button
+  const onButtonClick = () => {
+    // using Java Script method to get PDF file
+    fetch('Resume.pdf').then(response => {
+      response.blob().then(blob => {
+        // Creating new object of PDF file
+        const fileURL = window.URL.createObjectURL(blob);
+        // Setting various property values
+        let alink = document.createElement('a');
+        alink.href = fileURL;
+        alink.download = 'Resume.pdf';
+        alink.click();
+      })
+    })
+  }
   return (
-    <div className='project'> 
-        <Flip duration={1500}bottom><div className="title">Resume</div></Flip>
+    <div className='project'>
+      <Flip duration={1500} bottom><div className="title">Resume</div></Flip>
+
+      <Fade delay={200} duration={1500} >
+      <center>
+        
+        <div className='resumeTxt'>Click on below button to download the resume</div>
+        <button onClick={onButtonClick} className='download'>
+          Download PDF
+        </button>
+      </center></Fade>
     </div>
   )
 }
