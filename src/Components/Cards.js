64ac1e518c1a8c0bd3b47e607138f6c169dabb87
Sarrthi <sarrthijasrotia05@gmail.com>
2022-11-06 20:07:22 -0600

    added a tech used section to the project cards

--- src/Components/Cards.js
+++ src/Components/Cards.js
@@ -14,6 +14,7 @@ const Cards = (props) => {
       {/* btns and text */}
       <div className="cardText">
         <div className='projectTitle'>{props.title}</div>
+        <div className='techUsed'><span class='techTitle'>Tech used:</span> {props.techUsed}</div>
         <div className='linkBox'>
             <a href={props.github} className='links' target="_blank">Github</a>
             <a href={props.app}className='links' target="_blank">Live App</a>
--- src/Components/Projects.js
+++ src/Components/Projects.js
@@ -9,16 +9,16 @@ const Project =()=>(
     <Flip duration={1500}bottom><div className="title">Projects</div></Flip>
   <Fade delay={500} duration={2000}><div className="projectGrid">
     {/* project 1 */}
-  <Cards img={CoTeach} title={'CoTeach'} github={'https://github.com/SarrthiJasrotia/CoTeach_frontend.git'} app={'https://vocal-figolla-43a5cd.netlify.app/'} />
+  <Cards img={CoTeach} title={'CoTeach'} techUsed={''} github={'https://github.com/SarrthiJasrotia/CoTeach_frontend.git'} app={'https://vocal-figolla-43a5cd.netlify.app/'} />
   
   {/* project 2 */}
-  <Cards img={Itravelynot} title={'ITravelYNot'} github={'https://github.com/SarrthiJasrotia/Project_ITravelYNot.git'} app={'https://itravelynot.com/'} />
+  <Cards img={Itravelynot} title={'ITravelYNot'} techUsed={' React , MongoDB, Express, CSS, Google Firebase'} github={'https://github.com/SarrthiJasrotia/Project_ITravelYNot.git'} app={'https://itravelynot.com/'} />
   
   {/* project 3 */}
-  <Cards img={TravelBlog} title={'TravelBlog'} github={'https://github.com/SarrthiJasrotia/Project_ITravelYNot.git'} app={'https://travelblog-362615.uc.r.appspot.com/'} />
+  <Cards img={TravelBlog} title={'TravelBlog'} techUsed={''} github={'https://github.com/SarrthiJasrotia/Project_ITravelYNot.git'} app={'https://travelblog-362615.uc.r.appspot.com/'} />
   
   {/* project 4 */}
-  <Cards img={Portfolio} title={'Portfolio site'} github={'https://github.com/SarrthiJasrotia/portfolio.git'} app={'https://sarrthijasrotia.com/'} />
+  <Cards img={Portfolio} title={'Portfolio site'} techUsed={''} github={'https://github.com/SarrthiJasrotia/portfolio.git'} app={'https://sarrthijasrotia.com/'} />
   {/* <Cards img={''} title={''} github={''} app={''} /> */}
   {/* <Cards img={''} title={''} github={''} app={''} /> */}
   {/* <Cards img={''} title={''} github={''} app={''} /> */}
