1eb4a5be9ce2a91c4f05b14f4acf3b2904d439df
Sarrthi <sarrthijasrotia05@gmail.com>
2023-02-27 20:14:26 -0600

    added new skills to the tech stack

--- src/Components/TechStack.js
+++ src/Components/TechStack.js
@@ -8,6 +8,10 @@ import html from '../Images/html-5.png'
 import css from '../Images/css-3.png'
 import git from '../Images/git-original.svg'
 import Django from '../Images/Django.svg'
+import nextjs from '../Images/nextjs.svg'
+import tailwind from '../Images/tailwind.svg'
+
+
 const TechStack = () => {
   return (
     <div className='TechStack'> <h1>Skills</h1>
@@ -15,10 +19,13 @@ const TechStack = () => {
     <div><img src={react}/>REACT.JS</div>
     <div><img src={python}/>PYTHON</div>
     <div><img src={node}/>NODE.JS</div>
+    <div><img src={nextjs}/>NEXT.JS</div> 
     <div><img src={mongo}/>MONGODB</div>
     <div><img src={js}/>JAVASCRIPT</div>
     <div><img src={html}/>HTML 5</div>
     <div><img src={css}/>CSS 3</div> 
+    <div><img src={tailwind}/>TAILWIND</div> 
+
     <div><img src={Django}/>DJANGO</div>   
     <div><img src={git}/>GIT</div> 
     </div>
